36605dc83e809e2eeedea3154c51e71a864689ee
Sean Yi <seantuokc@gmail.com>
2020-11-10 22:21:34 +0900

    Stuff for data. Need to process validation data as well.

--- src/data.py
+++ src/data.py
@@ -1,5 +1,12 @@
+import logging
+import os
+
+import numpy as np
 import sentencepiece as spm
 from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
+
+logger = logging.getLogger()
 
 
 class WMT2014Dataset(Dataset):
@@ -8,34 +15,67 @@ class WMT2014Dataset(Dataset):
         self.max_seq_len = self.args.max_seq_len
 
         self.data = self.load()
+
+        if self.args.debug:
+            self.data = self.data[:100]
+
+        ############# Load/train tokenizer. #############################################################################
+        self.tokenizer = spm.SentencePieceProcessor()
+        self.tokenizer_path = os.path.join(self.args.data_root, self.args.tokenizer_filename)
+        try:
+            self.tokenizer.load(self.tokenizer_path + '.model')
+        except OSError:
+            logger.info("Training tokenizer and saving in %s" % self.args.tokenizer_filename)
+            train_command = '--input={} --model_prefix={} --vocab_size={} --bos_id=2 --eos_id=3 --pad_id=0 --unk_id=1'
+            train_file = ','.join([self.args.src_train_file, self.args.tgt_train_file])
+            spm.SentencePieceTrainer.train(train_command.format(train_file, self.tokenizer_path, self.args.vocab_size))
+            self.tokenizer.load(self.tokenizer_path + '.model')
+        #################################################################################################################
+
+        self.tokenized_data = self.tokenize()
+
+        src_longest = max([len(x[0]) for x in self.tokenized_data])
+        tgt_longest = max([len(x[1]) for x in self.tokenized_data])
+        print('==================================================================================================')
+        print("len(self.data) = {}".format(len(self.data)))
+        print('--------------------------------------------------------------------------------------------------')
+        print("Longest sequence for src length: {}".format(src_longest))
+        print("Longest sequence for src sentence: {}".format(self.data[np.argmax([len(x[0]) for x in self.tokenized_data])][0]))
+        print('--------------------------------------------------------------------------------------------------')
+        print("Shortest sequence for src length: {}".format(min([len(x[0]) for x in self.tokenized_data])))
+        print("Shortest sequence for src sentence: {}".format(self.data[np.argmin([len(x[0]) for x in self.tokenized_data])][0]))
+        print('--------------------------------------------------------------------------------------------------')
+        print("Longest sequence for tgt length: {}".format(tgt_longest))
+        print("Longest sequence for tgt sentence: {}".format(self.data[np.argmax([len(x[1]) for x in self.tokenized_data])][1]))
+        print('--------------------------------------------------------------------------------------------------')
+        print("Shortest sequence for src length: {}".format(min([len(x[1]) for x in self.tokenized_data])))
+        print("Shortest sequence for src sentence: {}".format(self.data[np.argmin([len(x[1]) for x in self.tokenized_data])][1]))
+        print('==================================================================================================')
+
         import pdb; pdb.set_trace()
 
-        self.tokenizer = spm.SentencePieceProcessor()
-        try:
-            self.tokenizer.load(args.tokenizer_file)
-        except OSError:
-            train_command = '--input={} --model_prefix={} --vocab_size={} --bos_id=2 --eos_id=3 --pad_id=0 --unk_id=1'
-            train_file = ','.join([self.args.src_file, self.args.tgt_file])
-            spm.SentencePieceTrainer.train(train_command.format(train_file, self.args.tokenizer_file, self.args.vocab))
-            self.tokenizer.load(args.tokenizer_file)
+        self.max_seq_len = max(src_longest, tgt_longest)
 
     def __len__(self):
-        pass
+        return len(self.tokenized_data)
 
     def __getitem__(self, idx):
-        pass
+        return self.tokenized_data[idx]
 
     def load(self):
-        with open(file=self.args.src_file, mode='r', encoding='utf-8') as f:
+        logger.info("Loading src and tgt from %s | %s" % (self.args.src_train_file, self.args.tgt_train_file))
+        with open(file=self.args.src_train_file, mode='r', encoding='utf-8') as f:
             src_data = [line.lower().strip() for line in f.readlines()]
 
-        with open(file=self.args.tgt_file, mode='r', encoding='utf-8') as f:
+        with open(file=self.args.tgt_train_file, mode='r', encoding='utf-8') as f:
             tgt_data = [line.lower().strip() for line in f.readlines()]
 
-        return list(zip(src_data, tgt_data))
+        return [[src, tgt] for src, tgt in zip(src_data, tgt_data)]
 
-    def tokenize(self, data):
-        pass
+    def tokenize(self):
+        logger.info("Tokenizing data...")
+        progress_bar = tqdm(iterable=self.data, desc="Tokenizing data", total=len(self.data))
+        return [[self.tokenizer.EncodeAsIds(src), self.tokenizer.EncodeAsIds(tgt)] for src, tgt in progress_bar]
 
     def process(self, data):
         pass
--- src/main.py
+++ src/main.py
@@ -1,24 +1,31 @@
 import argparse
+import logging
 import os
 
 import sentencepiece as spm
 
 from data import WMT2014Dataset
 
+logger = logging.getLogger()
+
 
 def main(args):
-    import pdb; pdb.set_trace()
+    msg_format = '[%(asctime)s - %(levelname)s - %(filename)s: %(lineno)d (%(funcName)s)] %(message)s'
+    logging.basicConfig(format=msg_format, level=logging.INFO, handlers=[logging.StreamHandler()])
+
     data = WMT2014Dataset(args)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_root', default='../data/', type=str)
-    parser.add_argument('--src_file', default='../data/train.fr-en_preprocessed.fr', type=str)
-    parser.add_argument('--tgt_file', default='../data/train.fr-en_preprocessed.en', type=str)
-    parser.add_argument('--dev_file', default='../data/valid.fr-en_preprocessed.fr', type=str)
-    parser.add_argument('--max_seq_len', default=100, type=int)
-    parser.add_argument('--tokenizer_file', default='../sentence_piece.model', type=str)
+    parser.add_argument('--debug', action='store_true', default=False)
+    parser.add_argument('--src_train_file', default='../data/train.fr-en_preprocessed.fr', type=str)
+    parser.add_argument('--tgt_train_file', default='../data/train.fr-en_preprocessed.en', type=str)
+    parser.add_argument('--src_dev_file', default='../data/valid.fr-en_preprocessed.fr', type=str)
+    parser.add_argument('--tgt_dev_file', default='../data/valid.fr-en_preprocessed.en', type=str)
+    parser.add_argument('--vocab_size', default=20000, type=int)
+    parser.add_argument('--tokenizer_filename', default='sentence_piece', type=str)
     args = parser.parse_args()
 
     main(args)
