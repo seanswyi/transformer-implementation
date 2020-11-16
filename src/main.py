4a9a4d65beb8cd51c40706143630024f3b5efa71
seanswyi <seantuokc@gmail.com>
2020-11-16 14:26:55 +0900

    Added in some lines for model testing.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import os
 import sentencepiece as spm
 
 from data import WMT2014Dataset
+from models.transformer import Transformer
 
 logger = logging.getLogger()
 
@@ -14,12 +15,20 @@ def main(args):
     logging.basicConfig(format=msg_format, level=logging.INFO, handlers=[logging.StreamHandler()])
 
     data = WMT2014Dataset(args)
+    model = Transformer(args)
+    thing = model(data.src_train_data[0])
+    # Run the embedded version into single head attention.
+    import pdb; pdb.set_trace()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_root', default='../data/', type=str)
     parser.add_argument('--debug', action='store_true', default=False)
+    parser.add_argument('--d_model', default=512, type=int)
+    parser.add_argument('--d_k', default=512, type=int)
+    parser.add_argument('--d_v', default=512, type=int)
+    parser.add_argument('--num_heads', default=8, type=int)
     parser.add_argument('--src_train_file', default='../data/train.fr-en_preprocessed.fr', type=str)
     parser.add_argument('--tgt_train_file', default='../data/train.fr-en_preprocessed.en', type=str)
     parser.add_argument('--src_valid_file', default='../data/valid.fr-en_preprocessed.fr', type=str)
