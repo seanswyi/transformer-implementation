a86034f7ebca1116760799141cd4454b5fecbc8f
seanswyi <seantuokc@gmail.com>
2020-11-18 14:53:40 +0900

    Created Decoder module.

---
+++ src/models/decoder.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+from models.feedforwardnn import FeedForwardNN
+from models.multihead_attention import MultiHeadAttention
+
+
+class Decoder(nn.Module):
+    def __init__(self, args):
+        super(Decoder, self).__init__()
+        self.ffnn = FeedForwardNN(args)
+        self.layer = nn.LayerNorm(normalized_shape=args.d_model)
+        self.multihead_attention = MultiHeadAttention(args)
+        self.masked_multihead_attention = MultiHeadAttention(args, mask=True)
+
+    def forward(self, x, enc_x):
+        attn_output = self.masked_multihead_attention(x, x, x)
+        output1 = self.layernorm(x + attn_output)
+
+        attn_output2 = self.multihead_attention(enc_x, enc_x, x)
+        output2 = self.layernorm(attn_output2 + output1)
+
+        output3 = self.layernorm(output2 + self.ffnn(output2))
+
+        return output3
