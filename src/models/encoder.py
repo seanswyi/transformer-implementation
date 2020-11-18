85f05d8a229109b48a0c6fe6494714ff9aa36609
seanswyi <seantuokc@gmail.com>
2020-11-18 14:53:15 +0900

    Changed attribute multiheadattention to multihead_attention.

--- src/models/encoder.py
+++ src/models/encoder.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 
 from models.feedforwardnn import FeedForwardNN
@@ -10,10 +9,10 @@ class Encoder(nn.Module):
         super(Encoder, self).__init__()
         self.ffnn = FeedForwardNN(args)
         self.layernorm = nn.LayerNorm(normalized_shape=args.d_model)
-        self.multiheadattention = MultiHeadAttention(args)
+        self.multihead_attention = MultiHeadAttention(args)
 
     def forward(self, x):
-        attn_output = self.multiheadattention(x, x, x)
+        attn_output = self.multihead_attention(x, x, x)
         output1 = self.layernorm(x + attn_output)
         output2 = self.layernorm(output1 + self.ffnn(output1))
 
