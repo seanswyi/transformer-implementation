5f9db8f31f8398bd8970922914d7a7105e55c1e6
seanswyi <seantuokc@gmail.com>
2020-11-20 21:00:20 +0900

    Fixed typos.

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -2,6 +2,7 @@ import torch.nn as nn
 
 from models.embedding_layer import EmbeddingLayer
 from models.encoder import Encoder
+from models.decoder import Decoder
 
 
 class Transformer(nn.Module):
@@ -10,9 +11,14 @@ class Transformer(nn.Module):
 
         self.emb = EmbeddingLayer(args)
         self.encoder = Encoder(args)
+        self.decoder = Decoder(args)
+        self.output_linear = nn.Linear(in_features=args.d_model, out_features=args.vocab_size)
 
-    def forward(self, x):
-        x = self.emb(x.long())
-        output = self.encoder(x)
+    def forward(self, src, tgt):
+        src_emb = self.emb(src.long())
+        tgt_emb = self.emb(tgt.long())
+        encoder_output = self.encoder(src_emb)
+        decoder_output = self.decoder(tgt_emb, encoder_output)
+        output = self.output_linear(decoder_output)
 
         return output
