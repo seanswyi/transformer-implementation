a072251eda37333d7060e521e41241a279fc8b1c
seanswyi <seantuokc@gmail.com>
2020-11-20 21:06:43 +0900

    Changed code to give stacks. Need to debug.

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -10,8 +10,8 @@ class Transformer(nn.Module):
         super(Transformer, self).__init__()
 
         self.emb = EmbeddingLayer(args)
-        self.encoder = Encoder(args)
-        self.decoder = Decoder(args)
+        self.encoder_stack = nn.ModuleList([Encoder(args) for _ in args.num_stacks])
+        self.decoder_stack = nn.ModuleList([Decoder(args) for _ in args.num_stacks])
         self.output_linear = nn.Linear(in_features=args.d_model, out_features=args.vocab_size)
 
     def forward(self, src, tgt):
@@ -20,5 +20,23 @@ class Transformer(nn.Module):
         encoder_output = self.encoder(src_emb)
         decoder_output = self.decoder(tgt_emb, encoder_output)
         output = self.output_linear(decoder_output)
+
+        return output
+
+    def encode(self, src):
+        src_emb = self.emb(src.long())
+
+        output = src_emb
+        for encoder in self.encoder_stack:
+            output = encoder(output)
+
+        return output
+
+    def decoder(self, tgt, enc_output):
+        tgt_emb = self.emb(tgt.long())
+
+        output = tgt_emb
+        for decoder in self.decoder_stack:
+            output = decoder(tgt_emb, enc_output)
 
         return output
