fb9ef6e6c1e753dd2fa31b94a8df4029474ad426
seanswyi <seantuokc@gmail.com>
2020-12-01 16:22:43 +0900

    Added in evaluation function. Need to check if it works properly.

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ import logging
 import os
 
 import torch
+import torch.multiprocessing as mp
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
 
 from data import WMT2014Dataset
 from models.transformer import Transformer
-from utils import adjust_learning_rate
+from utils import adjust_learning_rate, calculate_bleu
 
 logger = logging.getLogger()
 
@@ -20,15 +22,17 @@ def train(args, model, data):
     else:
         logger.warning("Not using GPU!")
 
-    for step in range(args.warmup_steps):
-        learning_rate = adjust_learning_rate(step, args)
+    model.train()
+
+    for step in tqdm(iterable=range(args.warmup_steps), desc="Steps", total=args.warmup_steps):
+        learning_rate = adjust_learning_rate(step, args) if args.learning_rate == 0 else 5e-3
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.Adam(params=model.parameters(), lr=learning_rate)
 
         step_loss = 0.0
 
-        progress_bar = tqdm(iterable=data.train_data, desc="Training", total=len(data.train_data))
-        for batch in progress_bar:
+        # progress_bar = tqdm(iterable=data.train_data, desc="Training", total=len(data.train_data))
+        for batch in tqdm(iterable=data.train_data, desc="Training", total=len(data.train_data)):
             optimizer.zero_grad()
 
             src, tgt = batch[:, 0], batch[:, 1]
@@ -56,13 +60,43 @@ def train(args, model, data):
     return 0
 
 
+def evaluate(args, model, data, tokenizer):
+    model.eval()
+
+    with torch.no_grad():
+        for step, batch in enumerate(data.valid_data):
+            src, tgt = batch[:, 0], batch[:, 1]
+            bos_tokens = torch.ones(size=(tgt.shape[0],)).reshape(-1, 1) * 2
+            tgt_shifted_right = torch.cat((bos_tokens, tgt), dim=1)[:, :-1]
+
+            if torch.cuda.is_available():
+                src = src.to('cuda')
+                tgt = tgt.to('cuda')
+                tgt_shifted_right = tgt_shifted_right.to('cuda')
+            else:
+                logger.warning("Not using GPU!")
+
+            output = model(src, tgt_shifted_right)
+            bleu_score = calculate_bleu(output, tgt, tokenizer)
+            logger.info(f"Step: {step} | BLEU: {bleu_score}")
+
+
 def main(args):
     msg_format = '[%(asctime)s - %(levelname)s - %(filename)s: %(lineno)d (%(funcName)s)] %(message)s'
     logging.basicConfig(format=msg_format, level=logging.INFO, handlers=[logging.StreamHandler()])
 
     data = WMT2014Dataset(args)
     model = Transformer(args)
+
+    if args.multiple_gpu:
+        logger.info("Using multiple GPU's!")
+        os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
+        model = nn.DataParallel(model)
+
+    model = model.to('cuda')
     results = train(args, model, data)
+    evaluate(args, model, data, data.tokenizer)
+
     # Run the embedded version into single head attention.
     import pdb; pdb.set_trace()
 
@@ -80,6 +114,8 @@ if __name__ == '__main__':
     parser.add_argument('--d_k', default=512, type=int)
     parser.add_argument('--d_v', default=512, type=int)
     parser.add_argument('--log_step', default=50, type=int)
+    parser.add_argument('--learning_rate', default=0, type=float)
+    parser.add_argument('--multiple_gpu', action='store_true', default=False)
     parser.add_argument('--num_epochs', default=3, type=int)
     parser.add_argument('--num_heads', default=8, type=int)
     parser.add_argument('--num_stacks', default=6, type=int)
