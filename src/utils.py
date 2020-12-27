bbad974688d73ed5ce3e6eda19e2ceb2cacf7b3d
Sean Yi <seantuokc@gmail.com>
2020-12-27 13:44:37 +0900

    Removed unnecessary functions, added an extra blank line before logger level-0, and fixed stuff I can't remember.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,5 @@
-from nltk.translate.bleu_score import sentence_bleu
 import numpy as np
-# from sacrebleu import sentence_bleu
+from sacrebleu import sentence_bleu
 import torch
 import torch.nn.functional as F
 
@@ -11,6 +10,19 @@ def adjust_learning_rate(step_num, args):
     term2 = min(np.power(step_num, -0.5), step_num * np.power(args.warmup_steps, -1.5))
 
     return term1 * term2
+
+
+def calculate_bleu(predictions, targets, tokenizer):
+    predictions = predictions.long().tolist()
+    targets = targets.long().tolist()
+
+    predictions_decoded = [tokenizer.DecodeIds(ids) for ids in predictions]
+    targets_decoded = [tokenizer.DecodeIds(ids) for ids in targets]
+
+    bleu_scores = [sentence_bleu(prediction, [target]).score for prediction, target in zip(predictions_decoded, targets_decoded)]
+    final_bleu_score = sum(bleu_scores) / len(bleu_scores)
+
+    return final_bleu_score
 
 
 def decode_autoregressive(model, src):
@@ -28,34 +40,6 @@ def decode_autoregressive(model, src):
     return outputs[:, 1:]
 
 
-def calculate_bleu(predictions, targets, tokenizer):
-    predictions = predictions.long().tolist()
-    targets = targets.long().tolist()
-
-    predictions_decoded = [tokenizer.DecodeIds(ids) for ids in predictions]
-    targets_decoded = [tokenizer.DecodeIds(ids) for ids in targets]
-
-    bleu_scores = [sentence_bleu([target], prediction) for target, prediction in zip(targets_decoded, predictions_decoded)]
-    final_bleu_score = sum(bleu_scores) / len(bleu_scores)
-
-    return final_bleu_score
-
-
-def print_data_stats(og_data, tokenized_data):
-    src_longest = max([len(x[0]) for x in tokenized_data])
-    tgt_longest = max([len(x[1]) for x in tokenized_data])
-    print('==================================================================================================')
-    print("len(og_data) = {}".format(len(og_data)))
-    print('--------------------------------------------------------------------------------------------------')
-    print("Longest sequence for src length: {}".format(src_longest))
-    print("Longest sequence for src sentence: {}".format(og_data[np.argmax([len(x[0]) for x in tokenized_data])][0]))
-    print('--------------------------------------------------------------------------------------------------')
-    print("Shortest sequence for src length: {}".format(min([len(x[0]) for x in tokenized_data])))
-    print("Shortest sequence for src sentence: {}".format(og_data[np.argmin([len(x[0]) for x in tokenized_data])][0]))
-    print('--------------------------------------------------------------------------------------------------')
-    print("Longest sequence for tgt length: {}".format(tgt_longest))
-    print("Longest sequence for tgt sentence: {}".format(og_data[np.argmax([len(x[1]) for x in tokenized_data])][1]))
-    print('--------------------------------------------------------------------------------------------------')
-    print("Shortest sequence for src length: {}".format(min([len(x[1]) for x in tokenized_data])))
-    print("Shortest sequence for src sentence: {}".format(og_data[np.argmin([len(x[1]) for x in tokenized_data])][1]))
-    print('==================================================================================================')
+def translate(data, tokenizer):
+    data = data.long().tolist()
+    return [tokenizer.DecodeIds(ids) for ids in data]
