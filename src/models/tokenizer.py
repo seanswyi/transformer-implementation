20d037ec742d7168d1eeb98b7985611b92f1ee48
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-09-21 07:55:18 +0900

    New Tokenizer object.
    The previous code was using the tokenizer within the `WMT2014Dataset`
    object. This was problematic because we have to use tokenization for
    other things (e.g., translating text from the Transformer) but can't if
    it's coupled with the dataset.
    
    It also doesn't make that much sense semantically since a dataset
    doesn't need to have its own tokenizer.

---
+++ src/models/tokenizer.py
@@ -0,0 +1,41 @@
+import logging
+
+from sentencepiece import SentencePieceProcessor, SentencePieceTrainer
+
+
+logger = logging.getLogger()
+
+
+class Tokenizer(SentencePieceProcessor):
+    def __init__(
+        self,
+        tokenizer_name: str = None,
+        raw_text_files: str = None,
+        vocab_size: int = None,
+        tokenizer_algo: str = "bpe",
+    ):
+        super().__init__()
+
+        if tokenizer_name:
+            logger.info("Loading tokenizer model from %s", tokenizer_name)
+            self.load(tokenizer_name)
+        else:
+            if tokenizer_name.endswith(".model"):
+                tokenizer_name = tokenizer_name.replace(".model", "")
+
+            logger.info(
+                "Model not received, training from scratch using provided text data"
+            )
+
+            train_command = (
+                f"--input={raw_text_files} "
+                f"--model_prefix={tokenizer_name} "
+                f"--model_type={tokenizer_algo} "
+                f"--bos_id=2 "
+                f"--eos_id=3 "
+                f"--pad_id=0 "
+                f"--unk_id=1"
+            )
+            SentencePieceTrainer.train(train_command)
+
+            self.load(f"{tokenizer_name}.model")
