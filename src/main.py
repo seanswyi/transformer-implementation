45a146cff48c121ec367947588599c4d59f78354
Sean (Seok-Won) Yi <seantuokc@gmail.com>
2023-09-22 16:15:24 +0900

    Added arg and tokenizer.
    [In Progress] Added argument for tokenization algorithm and initialized
    a tokenizer object.
    
    [To-Do] Need to test whether the tokenizer can be called with a
    pre-trained model file + if we can properly train it from scratch.

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 from torch import nn
 
 from data import WMT2014Dataset
+from models.tokenizer import Tokenizer
 from models.transformer import Transformer
 from train import train
 
@@ -46,8 +47,14 @@ def main(args):
     args_msg = f"\t{args_msg}"
     logger.info("\nArguments:\n%s", args_msg)
 
+    tokenizer = Tokenizer(
+        tokenizer_name=args.tokenizer_name,
+        train_text_files=",".join([args.src_train_file, args.tgt_train_file]),
+        vocab_size=args.vocab_size,
+        tokenization_algo=args.tokenization_algo,
+    )
     data = WMT2014Dataset(args)
-    model = Transformer(args)
+    model = Transformer(args, tokenizer=tokenizer)
 
     if args.multiple_gpu:
         logger.info("Using multiple GPU's!")
@@ -216,6 +223,13 @@ if __name__ == "__main__":
         "--tokenizer_name",
         default="fr-en",
         type=str,
+    )
+    parser.add_argument(
+        "--tokenization_algo",
+        default="bpe",
+        type=str,
+        choices=["unigram", "bpe", "char", "word"],
+        help="Tokenization algorithm used to train tokenizer.",
     )
     parser.add_argument(
         "--wandb_name",
